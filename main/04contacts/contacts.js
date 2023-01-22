f02950591078bdab43e4824b63802605ede0ef33
Stefan Herrmann <99035284+StefanHerrmann1991@users.noreply.github.com>
2023-01-22 10:27:42 +0100

    add contact dialog start

--- main/04contacts/contacts.js
+++ main/04contacts/contacts.js
@@ -1,33 +1,54 @@
+
+let content = document.getElementById('content');
+let contacts = [];
+
+
 class Contact {
-    constructor(name) {
-      this.name = name;
-    }
+  constructor(name) {
+    this.name = name;
   }
-  
-  class ContactBook {
-    constructor() {
-      this.contacts = [];
-    }
-  
-    addContact(name) {
-      this.contacts.push(new Contact(name));
-    }
-  
-    sortContacts() {
-      this.contacts.sort((a, b) => {
-        if (a.name < b.name) return -1;
-        if (a.name > b.name) return 1;
-        return 0;
-      });
-    }
-  } 
+}
 
-  const contactBook = new ContactBook();
-  contactBook.addContact("Alice");
-  contactBook.addContact("Bob");
-  contactBook.addContact("Charlie");
-  
-  // Sort the contacts by name
-  contactBook.sortContacts();
+class ContactBook {
+  constructor() {
+    this.contacts = [];
+    console.log(this.contacts);
+  }
 
+  addContact(name) {
+    this.contacts.push(new Contact(name));
+  }
+
+  sortContacts() {
+    this.contacts.sort((a, b) => {
+      if (a.name < b.name) return -1;
+      if (a.name > b.name) return 1;
+      return 0;
+    });
+  }
+}
+
+const contactBook = new ContactBook();
+contactBook.addContact("Stefan Herrmann");
+
+
+// Sort the contacts by name
+contactBook.sortContacts();
+
+
+function openContactDialog() {
+  document.getElementById('addContactDialog').classList.remove('d-none');
+}
+
+function renderAddContact() {
+  content.innerHTML = `
+  <div>  
+  <div>
+  <img src="assets/img/navbar/logo.png"
   
+  
+  </div>
+  </div>
+  `;
+
+}
