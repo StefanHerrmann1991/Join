11d15f90c0167ff7308adb3262e694d0eba058a6
Stefan Herrmann <99035284+StefanHerrmann1991@users.noreply.github.com>
2023-04-06 04:28:42 +0200

    find earliest task function

--- main/01summary/summary.js
+++ main/01summary/summary.js
@@ -6,9 +6,18 @@ const boards = [
 ];
 
 
+
+let allTaskNumber = 0;
+
 async function initSummary() {
   includeHTML();
   await initAddTasks();
+  const earliestTask = findEarliestTask();
+  if (earliestTask !== null) {
+    console.log('Task with the earliest date:', earliestTask);
+  } else {
+    console.log('No tasks found in the future.');
+  }
   await renderSummary();
 }
 
@@ -23,40 +32,17 @@ async function renderSummary() {
 }
 
 function filterBoards(boardTaskArray, boardId) {
-  boardTaskArray = tasks.filter(t => t['board'] == `${boardId}`).length;
-  getId(boardId).innerHTML = boardTaskArray
-}
-
-function renderBoards() {
-  let boardsContent = getId('boards');
-  boardsContent.innerHTML = '';
-  for (let i = 0; i < boards.length; i++) {
-    boardId = boards[i]['boardId'];
-    boardTitle = boards[i]['boardTitle'];
-    boardName = boards[i]['boardName'];
-    boardsContent.innerHTML += `
-      <div class="board">
-      <div class="board-header">
-      <h2>${boardTitle}</h2>
-      <button onclick="openContainer('addTaksPopup'); initTasks()"><img src="/assets/img/plusButton.png"</button> 
-      </div>
-      <div id="${boardId}" class="board-task-container" ondrop="moveTo('${boardId}')" ondragover="allowDrop(event)"></div>
-      </div>
-         `
-    renderBoards(boardTitle, boardId);
+  boardTaskArray = filterTasks(boardId);
+  getId(boardId).innerHTML = boardTaskArray;
+  allTaskNumber += boardTaskArray;
+  getId('allTasks').innerHTML = allTaskNumber;
   }
-}
 
-
-function renderEachBoard(boardTaskArray, boardId) {
-  boardTaskArray = tasks.filter(t => t['board'] == `${boardId}`);
-  getId(`${boardId}`).innerHTML = '';
-  for (let i = 0; i < boardTaskArray.length; i++) {
-    const element = boardTaskArray[i];
-    const taskIndex = tasks.indexOf(element);
-    getId(`${boardId}`).innerHTML += boardTaskHTML(element, taskIndex);
+  function findEarliestTask() {
+    if (tasks.length === 0) return null; // No tasks found    
+    tasks.sort((a, b) => new Date(a.date) - new Date(b.date));
+    return tasks[0].date;
   }
-}
 
 async function initAddTasks() {
   setURL('https://stefan-herrmann.developerakademie.net/smallest_backend_ever');
@@ -64,76 +50,23 @@ async function initAddTasks() {
   tasks = JSON.parse(backend.getItem('tasks')) || [];
 }
 
-
-
-function filterTasksByBoard(boardId) {
-  return boardTaskArray = tasks.filter(t => t['board'] == `${boardId}`);
-}
-
-function countTasksInBoard(boardId) {
-  return filterTasksByBoard(boardId).length;
+function filterTasks(boardId) {
+ return tasks.filter(t => t['board'] == `${boardId}`).length;
 }
 
 function getEaliestTask(boardId) {
   const currentDate = new Date();
   const tasksInBoard = filterTasksByBoard(boardId);
   const futureTasks = tasksInBoard.filter(task => new Date(task.date) > currentDate);
-
   if (futureTasks.length === 0) {
     return null;
   }
-
-  return futureTasks.reduce((earliestTask, currentTask) => {
-    const currentTaskDate = new Date(currentTask.date);
-    const earliestTaskDate = new Date(earliestTask.date);
-
-    return currentTaskDate < earliestTaskDate ? currentTask : earliestTask;
-  });
-}
-
-function displayTaskSummary() {
-  boards.forEach(board => {
-    console.log(`Board ID: ${board.boardId}`);
-    console.log('Number of tasks in board:', countTasksInBoard(board.boardId));
-
-    const earliestTask = getEaliestTask(board.boardId);
-    if (earliestTask) {
-      console.log('Earliest task in the future for the specified board:', earliestTask);
-    } else {
-      console.log('No tasks found in the future for the specified board.');
-    }
-    console.log('\n');
-  });
-}
-
-
-/* 
-function countTasksInBoard(boardId) {
-  return tasks?.filter(task => task.board === boardId).length;
-}
-
-function findEarliestTaskInFuture() {
-  const currentDate = new Date();
-  const futureTasks = tasks.filter(task => new Date(task.date) > currentDate);
   
-  if (futureTasks.length === 0) {
-    return null;
-  }
-
   return futureTasks.reduce((earliestTask, currentTask) => {
     const currentTaskDate = new Date(currentTask.date);
     const earliestTaskDate = new Date(earliestTask.date);
-
     return currentTaskDate < earliestTaskDate ? currentTask : earliestTask;
   });
 }
 
-const boardId = 'board-0';
-console.log('Number of tasks in board:', countTasksInBoard(boardId));
 
-const earliestTask = findEarliestTaskInFuture();
-if (earliestTask) {
-  console.log('Earliest task in the future:', earliestTask);
-} else {
-  console.log('No tasks found in the future.');
-} */
