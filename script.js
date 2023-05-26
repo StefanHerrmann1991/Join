32466ae050d26e8a969282a89305f3f07794f560
Stefan Herrmann <99035284+StefanHerrmann1991@users.noreply.github.com>
2023-05-27 00:48:24 +0200

    completed documentation 2

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
+/** 
+ * A list of urgency levels, ordered from highest to lowest.
+ * @type {Array<string>}
+ */
 let urgencies = ['High', 'Medium', 'Low'];
+
+/**
+ * A list of categories, where each category is a JSON with properties for the topic, color, and index.
+ * @type {Array<JSON>}
+ */
 let categories = [{
     'topic': 'Sales',
     'color': 'FC71FF',
@@ -8,25 +17,80 @@ let categories = [{
     'color': '#1FD7C1',
     'index': '1'
 }];
-let users
-let tasks
+
+/** 
+ * A list of users.
+ * @type {Array<JSON>}
+ */
+let users;
+
+/** 
+ * A list of tasks.
+ * @type {Array<JSON>}
+ */
+let tasks;
+
+/**
+ * A list of subtasks.
+ * @type {Array<JSON>}
+ */
 let subtasks = [];
+
+/**
+ * A list of invited users.
+ * @type {Array<JSON>}
+ */
 let invitedUsers = [];
+
+/**
+ * A list of assigned users.
+ * @type {Array<JSON>}
+ */
 let assignedUsers = [];
-let colorPicker = ['#8AA4FF', '#FF0000', '#2AD300', '#FF8A00', '#E200BE', '#0038FF']
+
+/**
+ * An array of color codes for the color picker.
+ * @type {Array<string>}
+ */
+let colorPicker = ['#8AA4FF', '#FF0000', '#2AD300', '#FF8A00', '#E200BE', '#0038FF'];
+
+/**
+ * The chosen color from the color picker.
+ * @type {string}
+ */
 let chosenColor;
+
+/**
+ * The user who is currently logged in.
+ * @type {JSON}
+ */
 let loggedInUser;
+
+/**
+ * The chosen board by the logged in user.
+ * @type {JSON}
+ */
 let chosenBoard;
+
+/**
+ * A list of users who have registered on the platform.
+ * @type {Array<JSON>}
+ */
 let registeredUsers = [];
 
 
-
+/**
+ * Splits a string identifier into an array of substrings.
+ *
+ * @param {string} id - The identifier string to be split.
+ * @param {string} separator - The character or string that divides the identifier.
+ * @returns {Array} arrayOfStrings - The resulting array of substrings.
+ */
 function splitID(id, separator) {
     let arrayOfStrings = id.split(separator);
     return arrayOfStrings;
 }
 
-/* ********* generic functions ********* */
 
 /**
  * This function Returns an HTML element
@@ -37,6 +101,12 @@ function getId(id) {
     return document?.getElementById(id);
 }
 
+
+/**
+ * Returns the constructor name of a given parameter if it is an object, or returns the parameter itself.
+ * @param {Object|string} paramAsText - The parameter to be processed.
+ * @returns {string} The constructor name or the parameter itself.
+ */
 function newVariable(paramAsText) {
     let objectName;
     if (typeof paramAsText === 'object') objectName = paramAsText.constructor.name;
@@ -44,6 +114,11 @@ function newVariable(paramAsText) {
     return `${objectName}`;
 }
 
+
+/**
+ * Serializes an object and saves it to the backend under a key based on its constructor name.
+ * @param {Object} dataObject - The object to be saved to the backend.
+ */
 async function saveBackendDataOf(dataObject) {
     let wordAsText = newVariable(dataObject) + `AsText`;
     let stringifyDataObject = JSON.stringify(dataObject);
@@ -51,7 +126,11 @@ async function saveBackendDataOf(dataObject) {
 };
 
 
-
+/**
+ * Serializes a value and saves it to the backend under a given key.
+ * @param {string} key - The key under which the value will be saved.
+ * @param {string} value - The value to be serialized and saved.
+ */
 async function saveToBackend(key, value) {    
     if (event) event.preventDefault();
     keyAsText = JSON.stringify(value);
@@ -73,7 +152,7 @@ function loadFromBackend(key, value) {
 
 
 /**
- * This function Returns an array with one or several HTML elements
+ * This function returns an array with one or several HTML elements
  * @param {...string} idsArr - The id of one or several HTML elements
  * @returns {Object[]} elementArr - All corresponding HTML elements in an array
  */
@@ -85,6 +164,7 @@ function getIds(...idsArr) { // rest-operator
     return elementArr;
 }
 
+
 /**
  * This function Hides all passed HTML elements
  * @param  {...string} ids - The id of one or several HTML elements
@@ -93,6 +173,7 @@ function closeContainer(...ids) {
     if (ids.length == 1) getId(ids[0])?.classList.add('d-none');
     ids.forEach(id => getId(id)?.classList.add('d-none'));
 }
+
 
 /**
  * This function Displays all passed HTML elements
@@ -103,6 +184,7 @@ function openContainer(...ids) {
     ids.forEach(id => getId(id)?.classList.remove('d-none'));
 }
 
+
 /**
  * This function Toggles the view of all passed HTML elements
  * @param  {...string} ids - The id of one or several HTML elements
@@ -110,6 +192,7 @@ function openContainer(...ids) {
 function toggleContainer(...ids) {
     ids.forEach(id => getId(id)?.classList.toggle('d-none'));
 }
+
 
 /**
  * This Function Capitalizes the first letter of a string and returns the capitalized string
@@ -119,6 +202,7 @@ function toggleContainer(...ids) {
 function capitalizeFirst(str) {
     return str[0].toUpperCase() + str.slice(1);
 }
+
 
 /**
  * This function empties the values of all passed html form and input elements.
@@ -130,18 +214,36 @@ function clearInputValues(...elements) {
     }
 }
 
+
+/**
+ * Returns a new string with the first character of the input string converted to lowercase.
+ * @param {string} str - The string to be processed.
+ * @returns {string} The resulting string with the first character converted to lowercase.
+ */
 function lowerFirstLetter(str) {
     return str[0].toLowerCase() + str.slice(1);
 }
 
+
+/**
+ * Removes a given class from an HTML element identified by its ID.
+ * @param {string} element - The ID of the HTML element.
+ * @param {string} className - The class to be removed.
+ */
 function removeClass(element, className) {
     getId(`${element}`).classList.remove(className);
 }
+
+
+/**
+ * Adds a given class to an HTML element identified by its ID.
+ * @param {string} element - The ID of the HTML element.
+ * @param {string} className - The class to be added.
+ */
 function addClass(element, className) {
     getId(`${element}`).classList.add(className);
 }
 
-/* ****** ****** */
 
 /**
  * This function splits an array of strings and displays them nicely (with ', ' in between strings)
@@ -158,11 +260,19 @@ function displayStringsArr(stringsArray) {
     return strings;
 }
 
+
+/**
+ * Logs the user out by redirecting them to the login page.
+ */
 function logout() {
     window.open('../00login-register/login.html');
 }
 
 
+/**
+ * Checks whether the current window size corresponds to a mobile view.
+ * @returns {boolean} true if the current view is a mobile view (width <= 768px), false otherwise.
+ */
 function isMobileView() {
     return window.matchMedia('(max-width: 768px)').matches;
 }
